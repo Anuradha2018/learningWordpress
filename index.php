86465fb0b156caa1f02f63dc1bc0e6b827714467
Anuradha Mohanty <anuradhamohanty0@gmail.com>
2018-09-17 16:33:29 +0200

    third commit

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+get_header();
+
 if(have_posts()):
     while(have_posts()): the_post();?>
     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -12,5 +14,7 @@ else:
     echo '<p>No content found</p>';
 
 endif;
+
+get_footer();
 ?>
 
